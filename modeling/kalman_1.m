9ae377fc878d398a3f08530b0bb1cef72e8531b4
Robert Huston <robert.huston@pinpointdynamics.com>
2016-08-12 21:17:34 -0400

    Captured intermediate results for one-shot scenario.

--- modeling/kalman_1.m
+++ modeling/kalman_1.m
@@ -1,7 +1,9 @@
 function [ x, P ] = kalman_1(x, P, F, G, Q, z, H, R)
+  # Prediction:
   x_ = F * x;
   P_ = F * P * F' + G * Q * G';
 
+  # Correction:
   dz = z - H * x_;
   S = H * P_ * H' + R;
   K = P_ * H' / S;
